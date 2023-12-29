9a358379feceffc274fc60b1a167ca2d485004c4
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-12-29 13:37:02 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,21 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const body = document.body;
+    body.insertAdjacentHTML("beforeend", `
+    <div class="notification ${type}">
+      <h2 class="title">${title}</h2>
+      <p>${description}</p>
+    </div>
+  `);
+    const notification = document.querySelector(`.${type}`);
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    setTimeout(()=>{
+        notification.remove();
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
